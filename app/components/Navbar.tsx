14fb3b6c92ed3452d3cd3b72d76b3f5a20da525e
brf153 <153hsb@gmail.com>
2025-09-12 23:09:25 +0530

    add session to navbar
    Signed-off-by: brf153 <153hsb@gmail.com>

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,18 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import { NAVBAR_LINKS, WEBSITE_TEXT } from "../constants/constants";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 type Props = {};
 
-const Navbar = (props: Props) => {
+const Navbar = async (props: Props) => {
+  const session = await getServerSession(authOptions);
   return (
     <Box
       sx={{
@@ -26,15 +29,35 @@ const Navbar = (props: Props) => {
         boxSizing: "border-box",
         zIndex: 1000,
         p: 2,
-        pb: 3
+        pb: 3,
       }}
     >
-      <Box sx={{ display: "flex", flexDirection: "column", gap: "10px", width: "90%", margin: "0 auto" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          width: "90%",
+          margin: "0 auto",
+        }}
+      >
         <Box sx={{ display: "flex", flexDirection: "column", mb: 1 }}>
-          <Typography sx={{ color: "var(--color-text-primary)", fontSize: "32px", fontWeight: "bold", fontFamily: 'var(--font-family-bold)' }}>
+          <Typography
+            sx={{
+              color: "var(--color-text-primary)",
+              fontSize: "32px",
+              fontWeight: "bold",
+              fontFamily: "var(--font-family-bold)",
+            }}
+          >
             {WEBSITE_TEXT.name}
           </Typography>
-          <Typography sx={{ color: "var(--color-text-primary)", fontFamily: 'var(--font-family-primary)' }}>
+          <Typography
+            sx={{
+              color: "var(--color-text-primary)",
+              fontFamily: "var(--font-family-primary)",
+            }}
+          >
             {WEBSITE_TEXT.description}
           </Typography>
         </Box>
@@ -43,30 +66,66 @@ const Navbar = (props: Props) => {
 
         <Box sx={{ display: "flex", gap: "20px" }}>
           <Box sx={{ display: "flex", gap: "20px" }}>
-          {NAVBAR_LINKS.map((link) => (
-            <Box sx={{cursor: "pointer"}} key={link.label}>
-              <Link href={link.path} style={{ textDecoration: 'none', fontFamily: 'var(--font-family-primary)', color: 'var(--color-text-primary)' }}>
-                {link.label}
-              </Link>
-            </Box>
-          ))}
+            {NAVBAR_LINKS.map((link) => (
+              <Box sx={{ cursor: "pointer" }} key={link.label}>
+                <Link
+                  href={link.path}
+                  style={{
+                    textDecoration: "none",
+                    fontFamily: "var(--font-family-primary)",
+                    color: "var(--color-text-primary)",
+                  }}
+                >
+                  {link.label}
+                </Link>
+              </Box>
+            ))}
           </Box>
 
-          <Box sx={{ display: "flex", gap: "20px", marginLeft: "auto", alignItems: "center" }}>
-            <Typography sx={{ color: "var(--color-text-primary)", width: 'fit-content' }}>
+          <Box
+            sx={{
+              display: "flex",
+              gap: "20px",
+              marginLeft: "auto",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              sx={{ color: "var(--color-text-primary)", width: "fit-content" }}
+            >
               {WEBSITE_TEXT.number}
             </Typography>
             <FacebookIcon sx={{ color: "var(--color-text-primary)" }} />
             <InstagramIcon sx={{ color: "var(--color-text-primary)" }} />
             <LinkedInIcon sx={{ color: "var(--color-text-primary)" }} />
-            <Link href="/page/login" style={{ textDecoration: 'none' }}>
-              <Typography sx={{ color: "var(--color-text-primary)", display: 'flex', alignItems: 'center', gap: '5px', cursor: 'pointer' }}>
+            {session ? (
+              <Typography
+                sx={{
+                  color: "var(--color-text-primary)",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "5px",
+                }}
+              >
                 <AccountCircleIcon />
-                Log In
               </Typography>
-            </Link>
+            ) : (
+              <Link href="/page/login" style={{ textDecoration: "none" }}>
+                <Typography
+                  sx={{
+                    color: "var(--color-text-primary)",
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "5px",
+                    cursor: "pointer",
+                  }}
+                >
+                  <AccountCircleIcon />
+                  Log In
+                </Typography>
+              </Link>
+            )}
           </Box>
-
         </Box>
       </Box>
     </Box>
